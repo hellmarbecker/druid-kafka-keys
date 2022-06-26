11f9fc4f222e77ccec30c53dc4d503f1bc3d4ba4
hellmarbecker <becker@hellmar-becker.de>
2022-06-26 14:59:30 +0200

    Change format

--- generate-data.sh
+++ generate-data.sh
@@ -4,7 +4,7 @@ BROKER=localhost:9092
 TOPIC=keytest
 
 for i in {1..20}; do
-   echo key$i:message$i | kcat -b $BROKER -P -t $TOPIC -K ":"
+   echo 'key'$i'|{ "val": '$i' }' | kcat -b $BROKER -P -t $TOPIC -K "|"
    sleep $(( $RANDOM % 10 + 1 ))
 done
 
