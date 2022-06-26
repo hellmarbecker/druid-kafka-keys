e53f19d37816f5d78254b11a094b6cffe31d0d95
hellmarbecker <becker@hellmar-becker.de>
2022-06-26 10:53:44 +0200

    Working version

--- generate-data.sh
+++ generate-data.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+BROKER=localhost:9092
+TOPIC=keytest
+
 for i in {1..20}; do
-   echo message$i
+   echo key$i:message$i | kcat -b $BROKER -P -t $TOPIC -K ":"
    sleep $(( $RANDOM % 10 + 1 ))
 done
 
